5edf7f772b180dc65115f6655365adf5afa94fac
Shengtong <shengtonghan@gmail.com>
2018-11-12 12:26:50 -0600

    Nov122018

---
+++ analysis/shiny/app.R
@@ -0,0 +1,55 @@
+#It is very important that the name of the file is app.R, otherwise it would not be recognized as a Shiny app.
+#You should not have any R code after the shinyApp(ui = ui, server = server) line. That line needs to be the last line in your file.
+#It is good practice to place this app in its own folder, and not in a folder that already has other R scripts or files, unless those other files are used by your app.
+
+library(shiny)
+library(ggplot2)
+library(dplyr)
+
+bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
+
+ui <- fluidPage(
+  titlePanel("BC Liquor Store prices"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("priceInput", "Price", 0, 100, c(25, 40), pre = "$"),
+      radioButtons("typeInput", "Product type",
+                   choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+                   selected = "WINE"),
+      selectInput("countryInput", "Country",
+                  choices = c("CANADA", "FRANCE", "ITALY"))
+    ),
+    mainPanel(
+      plotOutput("coolplot"),
+      br(), br(),
+      tableOutput("results")
+    )
+  )
+)
+
+server <- function(input, output) {
+  output$coolplot <- renderPlot({
+    filtered <-
+      bcl %>%
+      filter(Price >= input$priceInput[1],
+             Price <= input$priceInput[2],
+             Type == input$typeInput,
+             Country == input$countryInput
+      )
+    ggplot(filtered, aes(Alcohol_Content)) +
+      geom_histogram()
+  })
+  
+  output$results <- renderTable({
+    filtered <-
+      bcl %>%
+      filter(Price >= input$priceInput[1],
+             Price <= input$priceInput[2],
+             Type == input$typeInput,
+             Country == input$countryInput
+      )
+    filtered
+  })
+}
+
+shinyApp(ui = ui, server = server)
