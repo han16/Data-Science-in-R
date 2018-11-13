fd915dffc686b5b8565657a9c2727b3b3bf6b16c
Shengtong <shengtonghan@gmail.com>
2018-11-13 11:08:34 -0600

    Nov132018

--- analysis/shiny/app.R
+++ analysis/shiny/app.R
@@ -16,8 +16,7 @@ ui <- fluidPage(
       radioButtons("typeInput", "Product type",
                    choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
                    selected = "WINE"),
-      selectInput("countryInput", "Country",
-                  choices = c("CANADA", "FRANCE", "ITALY"))
+      uiOutput("countryOutput")
     ),
     mainPanel(
       plotOutput("coolplot"),
@@ -28,27 +27,35 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-  output$coolplot <- renderPlot({
-    filtered <-
-      bcl %>%
+  output$countryOutput <- renderUI({
+    selectInput("countryInput", "Country",
+                sort(unique(bcl$Country)),
+                selected = "CANADA")
+  })
+
+  filtered <- reactive({
+    if (is.null(input$countryInput)) {
+      return(NULL)
+    }
+
+    bcl %>%
       filter(Price >= input$priceInput[1],
              Price <= input$priceInput[2],
              Type == input$typeInput,
              Country == input$countryInput
       )
-    ggplot(filtered, aes(Alcohol_Content)) +
+  })
+
+  output$coolplot <- renderPlot({
+    if (is.null(filtered())) {
+      return()
+    }
+    ggplot(filtered(), aes(Alcohol_Content)) +
       geom_histogram()
   })
-  
+
   output$results <- renderTable({
-    filtered <-
-      bcl %>%
-      filter(Price >= input$priceInput[1],
-             Price <= input$priceInput[2],
-             Type == input$typeInput,
-             Country == input$countryInput
-      )
-    filtered
+    filtered()
   })
 }
 
